c42c19098075a35e1c495fea307ef54c2579292f
sahilm-ti <sahil.marwaha@trilogy.com>
2026-01-16 20:38:31 +0530

    Change mail temporarily

--- src/alpha_analysis_downstream_processing_mcp/email_sender.py
+++ src/alpha_analysis_downstream_processing_mcp/email_sender.py
@@ -17,7 +17,8 @@ AWS_REGION = "us-east-1"  # Default AWS region for SES
 
 # LOI email recipients
 LOI_TO_ADDRESSES: list[str] = ["sahil.marwaha@trilogy.com"]
-LOI_CC_ADDRESSES: list[str] = ["greg.foote@trilogy.com"]
+# TODO: REVERT
+LOI_CC_ADDRESSES: list[str] = ["sahil.marwaha@trilogy.com"]
 
 
 class _SESClient(Protocol):
